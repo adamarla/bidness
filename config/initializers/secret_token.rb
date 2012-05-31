a492074fe5a2835f4ad4ad2dc208612cc3aa3c83
Akshay Damarla <akshay.damarla@gmail.com>
2012-05-31 10:58:32 +0530

    inital commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bidness::Application.config.secret_token = '2c06535d2e9baaf81c71037025e8c7819586436f09f97df2230d0ad72a702b8f0f79752770b9decc130021332e4c708ff528db88bc0d7be9a4db8e156bae8bdb'
